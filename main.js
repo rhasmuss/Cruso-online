b8f75910e187d145716dd881c5d7ddc39609474b
Ariel Claros <exdoog@gmail.com>
2019-09-27 17:06:01 -0400

    prueba de instansias

--- main.js
+++ main.js
@@ -33,15 +33,15 @@ var Estudiante = /** @class */ (function () {
     return Estudiante;
 }());
 exports.Estudiante = Estudiante;
-var Drieccion = /** @class */ (function () {
-    function Drieccion(ciud, ba, calle) {
+var Direccion = /** @class */ (function () {
+    function Direccion(ciud, ba, calle) {
         this.ciudad = ciud;
         this.barrio = ba;
         this.calle = calle;
     }
-    return Drieccion;
+    return Direccion;
 }());
-exports.Drieccion = Drieccion;
+exports.Direccion = Direccion;
 var Curso = /** @class */ (function () {
     function Curso(nom, dir, precio, doc) {
         this.nombre = nom;
@@ -56,8 +56,47 @@ var CompraCurso = /** @class */ (function () {
     function CompraCurso(fecha, curso, estud) {
         this.fecha = fecha;
         this.curso = curso;
-        this.estuante = estud;
+        this.estudiante = estud;
     }
     return CompraCurso;
 }());
 exports.CompraCurso = CompraCurso;
+var direcionn1 = new Direccion("cbba", 'carinito', 'el apsaje del diablo');
+var direcionn2 = new Direccion("la paz", 'caca de lugar', 'sin calle');
+var direcionn3 = new Direccion("santa cruj", 'plan30000', 'elay ps');
+var estudiante1 = new Estudiante('pedro', 'mamani', 'pedro@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante2 = new Estudiante('juan', 'quispe', 'juan@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante3 = new Estudiante('jose', 'claros', 'jose@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante4 = new Estudiante('luis', 'herbas', 'luis@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante5 = new Estudiante('carlos', 'chambi', 'carlos@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante6 = new Estudiante('miguel', 'guzman', 'miguel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante7 = new Estudiante('daniel', 'flores', 'daniel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante8 = new Estudiante('maria', 'rodriguez', 'maria@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante9 = new Estudiante('mariel', 'cepedez', 'mariel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var estudiante10 = new Estudiante('ariel', 'claros', 'ariel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var docente1 = new Docente('angel', 'antezana', 'ing', 'angel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var docente2 = new Docente('pepito', 'el cacha viejas', 'ing', 'pepito@gmail.com', [direcionn1, direcionn2, direcionn3]);
+var curso1 = new Curso('java', 'prado', 100, [docente1, docente2]);
+var curso2 = new Curso('angular', 'quillacollo', 670, [docente1, docente2]);
+var curso3 = new Curso('python', 'san martin', 450, [docente1, docente2]);
+var curso4 = new Curso('larabel', 'torre42', 800, [docente1, docente2]);
+var curso5 = new Curso('java script', 'thecnne', 500, [docente1, docente2]);
+var compra1 = new CompraCurso('12-3-19', curso1, estudiante1);
+var compra2 = new CompraCurso('1-7-2019', curso2, estudiante2);
+var compra3 = new CompraCurso('6-7-2018', curso3, estudiante3);
+var compra4 = new CompraCurso('25-8-2019', curso4, estudiante4);
+var compra5 = new CompraCurso('15-4-19', curso5, estudiante5);
+var compra6 = new CompraCurso('16-7-19', curso1, estudiante6);
+var compra7 = new CompraCurso('16-11-19', curso2, estudiante7);
+var compra8 = new CompraCurso('3-3-19', curso1, estudiante8);
+var compra9 = new CompraCurso('9-6-19', curso5, estudiante9);
+var compra10 = new CompraCurso('4-4-19', curso1, estudiante10);
+var compra11 = new CompraCurso('12-2-19', curso4, estudiante1);
+var compra12 = new CompraCurso('9-6-19', curso5, estudiante7);
+console.log(direcionn1);
+console.log(direcionn2);
+console.log(direcionn3);
+console.log(estudiante1, estudiante2, estudiante3, estudiante4, estudiante5, estudiante6, estudiante7, estudiante8, estudiante9, estudiante10);
+console.log(docente1, docente2);
+console.log(curso1, curso2, curso3, curso4, curso5);
+console.log(compra1, compra2, compra3, compra4, compra5, compra6, compra7, compra8, compra9, compra10, compra11, compra12);
--- main.ts
+++ main.ts
@@ -17,42 +17,34 @@ export class Docente {
     private apellido: string;
     private correo: string;
     private profecion: string;
-    private direcion: Drieccion[];
-
-    constructor(nom: string, ap: string, prof: string, corre: string, direc: Drieccion[]) {
+    private direcion: Direccion[];
+    constructor(nom: string, ap: string, prof: string, corre: string, direc: Direccion[]) {
         this.nombre = nom;
         this.apellido = ap;
         this.profecion = prof;
         this.correo = corre;
         this.direcion = direc;
-
     }
-
 }
 
 export class Estudiante {
-
     private nombre: string;
     private apellido: string;
     private correo: string;
-    private direccion: Drieccion[];
-
-    constructor(nom: string, ap: string, corre: string, dir: Drieccion[]) {
+    private direccion: Direccion[];
+    constructor(nom: string, ap: string, corre: string, dir: Direccion[]) {
         this.nombre = nom;
         this.apellido = ap;
         this.correo = corre;
         this.direccion = dir;
-
     }
-
 }
 
-export class Drieccion {
-
-    ciudad: string[];
-    barrio: string[];
-    calle: string[];
-    constructor(ciud: string[], ba: string[], calle: string[]) {
+export class Direccion {
+    ciudad: string;
+    barrio: string;
+    calle: string;
+    constructor(ciud: string, ba: string, calle: string) {
         this.ciudad = ciud;
         this.barrio = ba;
         this.calle = calle;
@@ -60,27 +52,64 @@ export class Drieccion {
 }
 export class Curso {
     nombre: string;
-    direccion: string[];
+    direccion: string;
     precio: number;
-    docente: Docente;
-    constructor(nom: string, dir: string[], precio: number, doc: Docente) {
+    docente: Docente[];
+    constructor(nom: string, dir: string, precio: number, doc: Docente[]) {
         this.nombre = nom;
         this.direccion = dir;
         this.precio = precio;
         this.docente = doc;
     }
-
 }
 
 export class CompraCurso {
-
     fecha: string;
     curso: Curso;
-    estuante: Estudiante;
+    estudiante: Estudiante;
     constructor(fecha: string, curso: Curso, estud: Estudiante) {
         this.fecha = fecha;
         this.curso = curso;
-        this.estuante = estud;
+        this.estudiante = estud;
     }
-
 }
+let direcionn1: Direccion = new Direccion("cbba", 'carinito', 'el apsaje del diablo');
+let direcionn2: Direccion = new Direccion("la paz", 'caca de lugar', 'sin calle');
+let direcionn3: Direccion = new Direccion("santa cruj", 'plan30000', 'elay ps');
+let estudiante1: Estudiante = new Estudiante('pedro', 'mamani', 'pedro@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante2: Estudiante = new Estudiante('juan', 'quispe', 'juan@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante3: Estudiante = new Estudiante('jose', 'claros', 'jose@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante4: Estudiante = new Estudiante('luis', 'herbas', 'luis@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante5: Estudiante = new Estudiante('carlos', 'chambi', 'carlos@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante6: Estudiante = new Estudiante('miguel', 'guzman', 'miguel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante7: Estudiante = new Estudiante('daniel', 'flores', 'daniel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante8: Estudiante = new Estudiante('maria', 'rodriguez', 'maria@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante9: Estudiante = new Estudiante('mariel', 'cepedez', 'mariel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let estudiante10: Estudiante = new Estudiante('ariel', 'claros', 'ariel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let docente1: Docente = new Docente('angel', 'antezana', 'ing', 'angel@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let docente2: Docente = new Docente('pepito', 'el cacha viejas', 'ing', 'pepito@gmail.com', [direcionn1, direcionn2, direcionn3]);
+let curso1: Curso = new Curso('java', 'prado', 100, [docente1, docente2]);
+let curso2: Curso = new Curso('angular', 'quillacollo', 670, [docente1, docente2]);
+let curso3: Curso = new Curso('python', 'san martin', 450, [docente1, docente2]);
+let curso4: Curso = new Curso('larabel', 'torre42', 800, [docente1, docente2]);
+let curso5: Curso = new Curso('java script', 'thecnne', 500, [docente1, docente2]);
+
+let compra1: CompraCurso = new CompraCurso('12-3-19', curso1, estudiante1);
+let compra2: CompraCurso = new CompraCurso('1-7-2019', curso2, estudiante2);
+let compra3: CompraCurso = new CompraCurso('6-7-2018', curso3, estudiante3);
+let compra4: CompraCurso = new CompraCurso('25-8-2019', curso4, estudiante4);
+let compra5: CompraCurso = new CompraCurso('15-4-19', curso5, estudiante5);
+let compra6: CompraCurso = new CompraCurso('16-7-19', curso1, estudiante6);
+let compra7: CompraCurso = new CompraCurso('16-11-19', curso2, estudiante7);
+let compra8: CompraCurso = new CompraCurso('3-3-19', curso1, estudiante8);
+let compra9: CompraCurso = new CompraCurso('9-6-19', curso5, estudiante9);
+let compra10: CompraCurso = new CompraCurso('4-4-19', curso1, estudiante10);
+let compra11: CompraCurso = new CompraCurso('12-2-19', curso4, estudiante1);
+let compra12: CompraCurso = new CompraCurso('9-6-19', curso5, estudiante7);
+console.log(direcionn1);
+console.log(direcionn2);
+console.log(direcionn3);
+console.log(estudiante1, estudiante2, estudiante3, estudiante4, estudiante5, estudiante6, estudiante7, estudiante8, estudiante9, estudiante10);
+console.log(docente1, docente2);
+console.log(curso1, curso2, curso3, curso4, curso5);
+console.log(compra1, compra2, compra3, compra4, compra5, compra6, compra7, compra8, compra9, compra10, compra11, compra12);
